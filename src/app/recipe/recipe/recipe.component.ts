e2bceda2f2f20e6a9c67987b6cb3ac6b7b75076d
Jacob Vacheresse <jvachere@umich.edu>
2020-10-29 14:02:03 -0400

    Fix recipe detail responsiveness and scrolling.

--- src/app/recipe/recipe/recipe.component.ts
+++ src/app/recipe/recipe/recipe.component.ts
@@ -1,6 +1,6 @@
 import { BaseComponent } from './../../shared/base-component';
 import { RecipeService } from './../recipe.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 
@@ -11,6 +11,8 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class RecipeComponent extends BaseComponent implements OnInit {
   recipe = null;
+  scrollThreshold = 0;
+  leftPaneFixed = true;
 
   constructor(
     public recipeService: RecipeService,
@@ -25,5 +27,65 @@ export class RecipeComponent extends BaseComponent implements OnInit {
       .subscribe(params =>
         this.baseSubscribe(this.recipeService.getRecipe(+params.id), (recipe) => this.recipe = recipe)
       );
+
+    const ingredientPane = document.getElementById('ingredient-pane');
+
+    this.onLoad(ingredientPane);
+
+    window.addEventListener('load', () => {
+      this.onLoad(ingredientPane);
+    });
+
+    window.addEventListener('resize', () => {
+      this.onResizeEvent(ingredientPane);
+    });
+
+    window.addEventListener('scroll', () => {
+      this.onScrollEvent(ingredientPane);
+    });
+  }
+
+  onLoad(ingredientPane): void {
+    if (window.innerWidth > 959) {
+      ingredientPane.classList.add('fixed');
+    }
+  }
+
+  onResizeEvent(ingredientPane): void {
+    if (window.innerWidth > 959) {
+      if (this.leftPaneFixed) {
+        ingredientPane.classList.add('fixed');
+        ingredientPane.classList.remove('scroll-margin');
+      }
+      else {
+        ingredientPane.classList.remove('fixed');
+        ingredientPane.classList.add('scroll-margin');
+      }
+    }
+    else {
+      ingredientPane.classList.remove('fixed');
+      ingredientPane.classList.remove('scroll-margin');
+    }
+
+    this.onScrollEvent(ingredientPane);
+  }
+
+  onScrollEvent(ingredientPane): void {
+    if (window.innerWidth > 959) {
+      if (this.leftPaneFixed) {
+        if (window.scrollY >= ((window.innerHeight - 84) * .6) - 232) {
+          ingredientPane.classList.remove('fixed');
+          ingredientPane.classList.add('scroll-margin');
+          this.leftPaneFixed = false;
+        }
+      }
+      else {
+        if (window.scrollY < ((window.innerHeight - 84) * .6) - 232) {
+          ingredientPane.classList.remove('scroll-margin');
+          ingredientPane.classList.add('fixed');
+          this.leftPaneFixed = true;
+        }
+      }
+    }
   }
 }
