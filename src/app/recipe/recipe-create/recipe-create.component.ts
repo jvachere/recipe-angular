16381a71ba883a367c5620673a6c88c111d1ebbc
Jacob Vacheresse <jvachere@umich.edu>
2020-12-03 16:22:10 -0500

    Update recipe create form

--- src/app/recipe/recipe-create/recipe-create.component.ts
+++ src/app/recipe/recipe-create/recipe-create.component.ts
@@ -1,33 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-recipe-create',
   templateUrl: './recipe-create.component.html',
   styleUrls: ['./recipe-create.component.scss']
 })
-export class RecipeCreateComponent implements OnInit {
-  recipeForm = new FormGroup({
-    name: new FormControl(''),
-    author: new FormControl(''),
-    description: new FormControl(''),
-    details: new FormControl(''),
-    prepTime: new FormControl(''),
-    bakeTime: new FormControl(''),
-    inactiveTime: new FormControl(''),
-    //tags: new FormControl(''),
-    quantity: new FormControl(''),
-    yieldString: new FormControl(''),
-    //recipeSteps: new FormControl(''),
-    //recipeIngredients: new FormControl(''),
+export class RecipeCreateComponent {
+  recipeForm = this.fb.group({
+    name: [''],
+    author: [''],
+    description: [''],
+    details: [''],
+    prepTime: [''],
+    bakeTime: [''],
+    inactiveTime: [''],
+    tags: this.fb.array([]),
+    quantity: [''],
+    yieldString: [''],
+    recipeIngredients: this.fb.array([]),
+    recipeSteps: this.fb.array([]),
   });
-
-  constructor() { }
-
-  ngOnInit(): void {
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  get tags(): FormArray {
+    return this.recipeForm.get('tags') as FormArray;
+  }
+  get recipeIngredients(): FormArray {
+    return this.recipeForm.get('recipeIngredients') as FormArray;
+  }
+  get recipeSteps(): FormArray {
+    return this.recipeForm.get('recipeSteps') as FormArray;
   }
 
-  onSubmit(): void{
+  constructor(private fb: FormBuilder) { }
+
+  addTag(event): void {
+    if ((event.value || '').trim()) {
+      this.tags.push(this.fb.control(event.value));
+    }
+    if (event.input) {
+      event.input.value = '';
+    }
+  }
+
+  removeTag(index): void {
+    this.tags.removeAt(index);
+
+  }
+
+
+
+  onSubmit(): void {
     console.warn(this.recipeForm.value);
   }
 }
